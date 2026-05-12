b16209e1f2cb07b0e1582a32f00bf72e667584db
Shreeharsh Patil <shreeharshgamerz@gmail.com>
2026-05-12 20:21:41 +0530

    Add for_loop_sum.py experiment

---
+++ for_loop_sum.py
@@ -0,0 +1,7 @@
+# WAP to display list of numbers and sum using for loop
+numbers_list = [1, 2, 3, 4, 5]
+total_sum = 0
+for i in numbers_list:
+    print(i)
+    total_sum = total_sum + i
+print("The sum is", total_sum)
