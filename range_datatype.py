3341e55543aa7c4b8f8b47f8bc7b0d3800a808ca
Shreeharsh Patil <shreeharshgamerz@gmail.com>
2026-05-12 20:30:41 +0530

    Add range_datatype.py experiment from expt2

---
+++ range_datatype.py
@@ -0,0 +1,10 @@
+# Python Program to create a range datatype
+r1 = range(10)
+print("Simple range(10):")
+for i in r1:
+    print(i)
+
+print("\nStep range(10, 20, 2):")
+r2 = range(10, 20, 2)
+for i in r2:
+    print(i)
