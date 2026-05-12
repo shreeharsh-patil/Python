aedcea78a146277aeab249e723bb15962387526b
Shreeharsh Patil <shreeharshgamerz@gmail.com>
2026-05-12 20:21:32 +0530

    Add digit_to_word.py experiment

---
+++ digit_to_word.py
@@ -0,0 +1,22 @@
+# To accept a numeric digit from the keyboard and display it in words
+x = int(input("Enter the number between 0 to 9: "))
+if x == 1:
+    print('One')
+elif x == 2:
+    print('Two')
+elif x == 3:
+    print('Three')
+elif x == 4:
+    print('Four')
+elif x == 5:
+    print('Five')
+elif x == 6:
+    print('Six')
+elif x == 7:
+    print('Seven')
+elif x == 8:
+    print('Eight')
+elif x == 9:
+    print('Nine')
+else:
+    print('Entered number is not in between 1 and 9 (or 0)')
