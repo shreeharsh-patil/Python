5d7074dfe6383f7a8fd5d9bc16b79aef68d1623b
Shreeharsh Patil <shreeharshgamerz@gmail.com>
2026-05-12 20:30:52 +0530

    Add type_operator.py experiment from expt2

---
+++ type_operator.py
@@ -0,0 +1,15 @@
+# Displaying Datatypes
+a = 2
+b = 4.2
+c = "shreeharsh"
+d = [3, 5, 'Shreeharsh']
+e = {2, 4, 3, 2}
+f = (3, 5, 'Shreeharsh')
+
+# Using type operator
+print("Type of a:", type(a))
+print("Type of b:", type(b))
+print("Type of c:", type(c))
+print("Type of d:", type(d))
+print("Type of e:", type(e))
+print("Type of f:", type(f))
