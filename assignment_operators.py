67bc20467da72bd076d8c44c44bed1638344919e
Shreeharsh Patil <shreeharshgamerz@gmail.com>
2026-05-12 20:20:59 +0530

    Add assignment_operators.py experiment

---
+++ assignment_operators.py
@@ -0,0 +1,64 @@
+# Arithmetic Operations
+a = 12
+b = 5
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Exponentiation:", a ** b)
+
+print("\nAssignment = Operator")
+x, y = 3, 4
+z = x + y
+print(z)
+
+print("\nAssignment += Operator")
+x, y = 3, 4
+z = x + y
+print(z)
+z += x
+print(z)
+
+print("\nAssignment -= Operator")
+x, y = 3, 4
+z = x - y
+print(z)
+z -= x
+print(z)
+
+print("\nAssignment *= Operator")
+x, y = 3, 4
+z = x * y
+print(z)
+z *= x
+print(z)
+
+print("\nAssignment /= Operator")
+x, y = 3, 4
+z = x / y
+print(z)
+z /= x
+print(z)
+
+print("\nAssignment %= Operator")
+x, y = 3, 4
+z = x % y
+print(z)
+z %= x
+print(z)
+
+print("\nAssignment **= Operator")
+x, y = 3, 4
+z = x ** y
+print(z)
+z **= x
+print(z)
+
+print("\nAssignment //= Operator")
+x, y = 10, 1
+z = x // y
+print(z)
+z //= x
+print(z)
