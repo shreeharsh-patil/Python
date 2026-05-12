52d743bde6aca0159b8bb2b7d6a80ea2fc635b7f
Shreeharsh Patil <shreeharshgamerz@gmail.com>
2026-05-12 20:21:21 +0530

    Add bitwise_operators.py experiment

---
+++ bitwise_operators.py
@@ -0,0 +1,26 @@
+# Bitwise complement operators
+x = 5
+print("~x:", ~x)
+
+print("\nBitwise AND operators")
+x = 5
+y = 4
+print("x & y:", x & y)
+
+print("\nBitwise OR operators")
+x = 5
+y = 4
+print("x | y:", x | y)
+
+print("\nBitwise XOR operators")
+x = 5
+y = 4
+print("x ^ y:", x ^ y)
+
+print("\nBitwise left shift operator")
+x = 5
+print("x << 2:", x << 2)
+
+print("\nBitwise right shift operator")
+x = 5
+print("x >> 2:", x >> 2)
