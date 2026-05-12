c4ffd826773ae14ac6b17d72a2fdbed4a281607d
Shreeharsh Patil <shreeharshgamerz@gmail.com>
2026-05-12 20:30:26 +0530

    Add string_datatype.py experiment from expt2

---
+++ string_datatype.py
@@ -0,0 +1,25 @@
+# Python Program for String Datatype
+my_str = "My name is 'Shreeharsh'"
+print(my_str)
+
+my_str2 = '''My Name is "Shreeharsh".I am From Verna'''
+print(my_str2)
+
+print("\nSlicing operators")
+# slicing operators
+s = "My Name is Shreeharsh.I am from Verna"
+print(s)
+print(s[0])
+print(s[3:7])
+print(s[11:])
+print(s[11:22])
+print(s[-1])
+
+print("\nRepetition operator")
+# Repetition operator
+s_rep = "Shreeharsh "
+print(s_rep)
+print(s_rep * 2)
+print(s_rep * 3)
+print(s_rep * 4)
+print(s_rep * 5)
