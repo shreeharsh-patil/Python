457ed18d97ba30a4464add0b78f8127cad5005e0
Shreeharsh Patil <shreeharshgamerz@gmail.com>
2026-05-12 20:30:23 +0530

    Add boolean_datatype.py experiment from expt2

---
+++ boolean_datatype.py
@@ -0,0 +1,25 @@
+# Python program for Bool datatype
+a = 10
+b = 20
+if a < b:
+    print("Hello World!")
+
+a = 10 > 20
+print(a)
+
+# Python program to add bool values
+print(True + True)
+print(True + False)
+
+print("\nFurther Boolean comparisons")
+# Python Program for bool datatype
+a = 100
+b = 200
+if a < b:
+    print("Shreeharsh")
+
+a = 10 > 5
+print(a)
+
+a = 10 > 50
+print(a)
